3ab37bd1fbbf8a81b73a95946841c90a18112cc0
AnyaKhabarova <76184706+AnyaKhabarova@users.noreply.github.com>
2021-02-25 09:56:12 -0800

    Ex.7.1.10_work with base keyword, inhereted method call

--- Exercises_7.1/Exercises_7.1/Program.cs
+++ Exercises_7.1/Exercises_7.1/Program.cs
@@ -25,4 +25,33 @@ namespace Exercises_7._1
             this.count = count;
         }
     }
+
+    class BaseClass
+    {
+        protected string Name;
+
+        public BaseClass(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public string Description;
+
+        public int Counter;
+
+        public DerivedClass(string name, string description): base(name)
+        {
+           
+            Description = description;
+        }
+
+        public DerivedClass(string name, string description, int counter): this(name, description)
+        {            
+            Counter = counter;
+        }
+    }
 }
+
