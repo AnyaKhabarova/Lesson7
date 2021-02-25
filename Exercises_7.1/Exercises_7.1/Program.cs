6822d5a79a71de8319ce23982d9a237f3f384358
AnyaKhabarova <76184706+AnyaKhabarova@users.noreply.github.com>
2021-02-24 20:44:23 -0800

    7.1.6

---
+++ Exercises_7.1/Exercises_7.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercises_7._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Obj
+    {
+        private string name;
+        private string owner;
+        private int length;
+        private int count;
+
+        public Obj(string name, string ownerName, int objLength, int count) 
+        {
+            this.name = name;
+            owner = ownerName;
+            length = objLength;
+            this.count = count;
+        }
+    }
+}
