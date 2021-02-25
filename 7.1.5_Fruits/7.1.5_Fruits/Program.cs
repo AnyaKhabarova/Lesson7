940ddd7f96b75efee6e6cbb23af98027b5b45df6
AnyaKhabarova <76184706+AnyaKhabarova@users.noreply.github.com>
2021-02-24 20:34:49 -0800

    Fruits and vegies classes

---
+++ 7.1.5_Fruits/7.1.5_Fruits/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _7._1._5_Fruits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Food { }
+    class Vegies: Food { }
+    class Fruits: Food { }
+    class Apple: Fruits { }
+    class Banana: Fruits { }
+    class Pear: Fruits { }
+    class Potato: Vegies { }
+    class Carrot: Vegies { }
+}
