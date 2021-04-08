1daba7473c1b4c0c49946a48ff827c783e7a7383
AnyaKhabarova <76184706+AnyaKhabarova@users.noreply.github.com>
2021-04-08 10:47:00 -0700

    Ex.7.1.7

---
+++ Ex.7.1.7/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+       class SmartHelper
+    {
+        private string name;
+
+        public SmartHelper(string name)
+        {
+            this.name = name;
+        }
+
+        public void Greetings(string name)
+        {
+            Console.WriteLine("Hi, {0}, I'm AI helper {1}", name, this.name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartHelper helper = new SmartHelper("Moses");
+            helper.Greetings("Anya");
+
+            Console.ReadKey();
+        }
+
+    }
+
+}
