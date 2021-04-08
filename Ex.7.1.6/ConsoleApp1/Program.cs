7e79a8b242ea4770f37a2b75dbc3381cf24d0460
AnyaKhabarova <76184706+AnyaKhabarova@users.noreply.github.com>
2021-04-08 10:40:09 -0700

    Ex.1.7.6

---
+++ Ex.7.1.6/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Obj
+    {
+        private string name;
+        private string owner;
+        private int length;
+        private int count;
+
+        public Obj(string name, string ownerName, int objLength, int count)
+        {
+            this.name = name;
+            owner = ownerName;
+            length = objLength;
+            this.count = count;
+
+        }
+    }
+}
